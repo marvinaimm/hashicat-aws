f2005c24917c30dfe8baee90b97dfb26880900f6
marvin <marvin.immaculata@ncs.com.sg>
2023-05-30 16:49:17 +0000

    Added s3 bucket module

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -1,5 +1,8 @@
-module "s3-bucket" {
+//--------------------------------------------------------------------
+// Modules
+module "s3_bucket" {
   source  = "app.terraform.io/marvin-org/s3-bucket/aws"
   version = "2.8.0"
+
   bucket_prefix = "marvin"
 }
